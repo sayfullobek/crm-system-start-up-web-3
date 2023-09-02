e6eaf1f971b8bce612986ceb0575db9653e7c83a
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-09-02 02:49:37 -0700

    v1

---
+++ src/test/java/it/ul/team/crmsystemstartup/CrmSystemStartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ul.team.crmsystemstartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSystemStartUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
