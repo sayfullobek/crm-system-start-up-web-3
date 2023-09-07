1839edffacc2b6621245f90f96549c6dff292599
user <dnorxolov@gmail.com>
2023-09-07 15:55:43 +0500

    Diyor

--- src/main/java/it/ul/team/crmsystemstartup/repository/CourseRepository.java
+++ src/main/java/it/ul/team/crmsystemstartup/repository/CourseRepository.java
@@ -3,6 +3,4 @@ package it.ul.team.crmsystemstartup.repository;
 import it.ul.team.crmsystemstartup.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-
-}
+public interface CourseRepository extends JpaRepository<Course, Integer> {}
