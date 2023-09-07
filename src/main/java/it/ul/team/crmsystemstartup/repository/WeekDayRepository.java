6cc714fd5bafb65dd2837e72fbc65afdea279537
Aziz ake <azizbekazizbek2033@gmail.com>
2023-09-07 19:28:21 +0500

    Bek

---
+++ src/main/java/it/ul/team/crmsystemstartup/repository/WeekDayRepository.java
@@ -0,0 +1,7 @@
+package it.ul.team.crmsystemstartup.repository;
+
+import it.ul.team.crmsystemstartup.entity.Week_day;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeekDayRepository extends JpaRepository<Week_day,Integer> {
+}
