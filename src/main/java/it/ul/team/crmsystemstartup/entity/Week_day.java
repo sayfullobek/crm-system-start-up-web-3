8eed6167dbf42af3c1dc1d8a27b5fc4f4ec6b161
Aziz ake <azizbekazizbek2033@gmail.com>
2023-09-07 19:24:43 +0500

    Aziz

--- src/main/java/it/ul/team/crmsystemstartup/entity/Week_day.java
+++ src/main/java/it/ul/team/crmsystemstartup/entity/Week_day.java
@@ -1,4 +1,23 @@
 package it.ul.team.crmsystemstartup.entity;
 
-public class Week_days {
+import it.ul.team.crmsystemstartup.entity.enums.WeekDaysName;
+import lombok.*;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Week_day {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(value = EnumType.STRING)
+    private WeekDaysName weekDaysName;
+
+    public Week_day(WeekDaysName weekDaysName) {
+        this.weekDaysName = weekDaysName;
+    }
 }
