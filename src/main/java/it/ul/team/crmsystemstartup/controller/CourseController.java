5a08ba1258d51cb5afd824ca208f50b72fdcbc49
Aziz ake <azizbekazizbek2033@gmail.com>
2023-09-08 20:49:24 +0500

    Aziz akeladan silaga ukalarim

--- src/main/java/it/ul/team/crmsystemstartup/controller/CourseController.java
+++ src/main/java/it/ul/team/crmsystemstartup/controller/CourseController.java
@@ -1,8 +1,11 @@
 package it.ul.team.crmsystemstartup.controller;
 
+import it.ul.team.crmsystemstartup.entity.Course;
+import it.ul.team.crmsystemstartup.exception.ResourceNotFoundException;
 import it.ul.team.crmsystemstartup.implement.controllerImplement.CourseControllerImpl;
 import it.ul.team.crmsystemstartup.payload.ApiResponse;
 import it.ul.team.crmsystemstartup.payload.CourseDto;
+import it.ul.team.crmsystemstartup.repository.CourseRepository;
 import it.ul.team.crmsystemstartup.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
@@ -17,9 +20,10 @@ import java.util.List;
 @CrossOrigin
 public class CourseController implements CourseControllerImpl {
     private final CourseService courseService;
+    private final CourseRepository courseRepository;
 
     @Override
-    @GetMapping
+    @GetMapping("/list")
     public HttpEntity<?> getCourse() {
         List<CourseDto> courseDtoList = courseService.getCourse();
         return ResponseEntity.ok(courseDtoList);
@@ -45,5 +49,12 @@ public class CourseController implements CourseControllerImpl {
     public HttpEntity<?> deleteCourse(@PathVariable Integer id) {
         ApiResponse<?> apiResponse = courseService.deleteCourse(id);
         return ResponseEntity.status(apiResponse.isSuccess()?200:409).body(apiResponse);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public HttpEntity<?> getOneCourse(@PathVariable Integer id) {
+        Course course = courseRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(404, "getCourse", "courseId",id));
+        return ResponseEntity.ok(course);
     }
 }
--- src/main/java/it/ul/team/crmsystemstartup/controller/GroupController.java
+++ src/main/java/it/ul/team/crmsystemstartup/controller/GroupController.java
@@ -1,8 +1,11 @@
 package it.ul.team.crmsystemstartup.controller;
 
+import it.ul.team.crmsystemstartup.entity.Group;
+import it.ul.team.crmsystemstartup.exception.ResourceNotFoundException;
 import it.ul.team.crmsystemstartup.implement.controllerImplement.GroupControllerImpl;
 import it.ul.team.crmsystemstartup.payload.ApiResponse;
 import it.ul.team.crmsystemstartup.payload.GroupDto;
+import it.ul.team.crmsystemstartup.repository.GroupRepository;
 import it.ul.team.crmsystemstartup.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
@@ -18,8 +21,17 @@ import java.util.UUID;
 @RequestMapping("/api/group")
 public class GroupController implements GroupControllerImpl {
     public final GroupService groupService;
+    public final GroupRepository groupRepository;
 
     @Override
+    @GetMapping("/{id}")
+    public HttpEntity<?> getOneGroup(@PathVariable UUID id) {
+        Group group = groupRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(404, "getGroup", "groupId", id));
+        return ResponseEntity.ok(group);
+    }
+
+    @Override
+    @GetMapping("/list")
     public HttpEntity<?> getGroup() {
         List<GroupDto> group = groupService.getGroup();
         return ResponseEntity.ok(group);
